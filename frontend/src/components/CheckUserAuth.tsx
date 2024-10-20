2bf7a442ce870dd593e1615b983baccaf1de5b37
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-10-20 06:27:32 +0530

    WIP: implement a way for auth provider  to send back tokens

--- frontend/src/components/CheckUserAuth.tsx
+++ frontend/src/components/CheckUserAuth.tsx
@@ -1,5 +1,6 @@
 import { useToast } from "@chakra-ui/react";
 import { ReactNode, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export type CheckUserAuthProps = {
   forComponent: ReactNode;
@@ -9,6 +10,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export function CheckUserAuth(props: CheckUserAuthProps) {
   const [isTokenValid, setIsTokenValid] = useState<boolean | null>(null);
   const toast = useToast();
+  const [urlSearchParams, setUrlSearchParams] = useSearchParams();
 
   /** Show a toast for login error */
   const toastLoginError = (reason?: string) =>
@@ -64,6 +66,17 @@ export function CheckUserAuth(props: CheckUserAuthProps) {
       toastLoginError(e.toString());
     }
   };
+
+  useEffect(() => {
+    if (urlSearchParams.get("auth_token")) {
+      localStorage.setItem(
+        "token",
+        urlSearchParams.get("auth_token") as string,
+      );
+      urlSearchParams.delete("auth_token");
+      setUrlSearchParams(urlSearchParams);
+    }
+  }, [urlSearchParams]);
 
   /** Check if the token is valid */
   useEffect(() => {
