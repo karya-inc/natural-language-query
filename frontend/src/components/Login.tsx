b20a68cf54ce8db1290690719e983b206bcfa25f
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-10-28 08:08:55 +0530

    feat: create a proper login card

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,44 @@
-import { Button, Center } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { BOT_NAME } from "../config";
+import { BotName } from "./RenderBotName";
 
-export function Login() {
+export type LoginProps = {
+  onLogin: () => void;
+};
+export function Login(props: LoginProps) {
   return (
     <Center height="100%" bg="gray.900">
-      <Button colorScheme="green">Sign In To Continue</Button>
+      <Card width="400px" bg="gray.700" padding={8} gap={5}>
+        <Heading
+          color="whiteAlpha.800"
+          fontWeight="bold"
+          fontSize="2xl"
+          textAlign="center"
+        >
+          Welcome to <BotName />
+        </Heading>
+        <Button
+          bgGradient="linear(to-r, impactGreen, #A8E55E, impactGreen)"
+          color="blackAlpha.800"
+          onClick={props.onLogin}
+          transition="all 0.4s ease-in-out"
+          _hover={{
+            backgroundPosition: "100% 0",
+          }}
+          backgroundSize="300% 300%"
+        >
+          Sign In
+        </Button>
+      </Card>
     </Center>
   );
 }
