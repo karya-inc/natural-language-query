1c69f185db99eeec8e25333909f340906e789fe0
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-11-09 13:00:23 +0530

    chore: use custom component for botname

--- frontend/src/pages/Chat/BotGreeting.tsx
+++ frontend/src/pages/Chat/BotGreeting.tsx
@@ -1,7 +1,6 @@
-import { Image, Heading, Box, HStack } from "@chakra-ui/react";
+import { Heading, HStack } from "@chakra-ui/react";
 import CFImage from "../../components/CloudflareImage";
-
-const BOT_NAME = "Kalai";
+import { BotName } from "../../components/RenderBotName";
 
 const BotGreeting = () => {
   return (
@@ -18,16 +17,7 @@ const BotGreeting = () => {
         alt="Karya logo"
       />
       <Heading color="gray.500" fontWeight="normal">
-        Hello, how can{" "}
-        <Box
-          as="span"
-          fontWeight="extrabold"
-          bgGradient="linear(to-br, impactGreen, #C8E56E)"
-          bgClip="text"
-        >
-          {BOT_NAME}
-        </Box>{" "}
-        help you today?
+        Hello, how can <BotName /> help you today?
       </Heading>
     </HStack>
   );
