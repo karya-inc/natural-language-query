b31041d81ab44ac230fed390b49eb72178bd0e8b
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-11-15 08:36:49 +0530

    chore: update openai env keys

--- backend/agents/azure_openai.py
+++ backend/agents/azure_openai.py
@@ -16,9 +16,9 @@ class AzureAIAgentTools(AgentTools):
 
     def __init__(self) -> None:
         self.az_ai_client = AzureOpenAI(
-            api_key=os.environ.get("AZURE_API_KEY"),
-            azure_endpoint=os.environ.get("AZURE_ENDPOINT", ""),
-            api_version=os.environ.get("AZURE_VERSION"),
+            api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
+            azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", ""),
+            api_version=os.environ.get("AZURE_OPENAI_API_VERSION"),
         )
 
         self.gemini_ai_client = OpenAI(
@@ -27,7 +27,9 @@ class AzureAIAgentTools(AgentTools):
         )
 
     @override
-    def invoke_llm[T](self, response_type: type[T], messages: list[ChatCompletionMessageParam]) -> T:
+    def invoke_llm[
+        T
+    ](self, response_type: type[T], messages: list[ChatCompletionMessageParam]) -> T:
 
         response = self.az_ai_client.beta.chat.completions.parse(
             model="gpt-4o", messages=messages, response_format=response_type
